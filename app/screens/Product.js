4c60921018d9f9fe6a7c4e887fc906635f6e0ae3
Keenan Thompson <keenan@keenant.com>
2017-10-09 12:21:36 -0500

    oops didnt commit

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -11,12 +11,20 @@ import {
   Image,
   View
 } from 'react-native';
-import { Card, List, ListItem, Button } from 'react-native-elements'
+import { Card, List, ListItem, Button, Icon } from 'react-native-elements'
 import App from "../App"
+
+const FoodStatuses = {
+  Loading: 0,
+  CanEat: 1,
+  CannotEat: 2,
+  Unknown: 3
+}
 
 export default class Home extends Component<{}> {
   state = {
     matches: [],
+    flashDismissed: false,
     code: undefined,
     product: {
       desc: "N/A",
@@ -27,7 +35,7 @@ export default class Home extends Component<{}> {
   }
 
   static navigationOptions = ({ navigation }) => ({
-    title: `${navigation.state.params.title}`,
+    title: `${navigation.state.params.title || ''}`,
   });
 
   componentWillMount = () => {
@@ -43,34 +51,113 @@ export default class Home extends Component<{}> {
       code
     })
 
-    fetch('http://192.168.0.104:8080/api/upc/' + code)
+    fetch('http://10.141.128.134:8080/api/upc/' + code)
       .then(res => res.json())
       .then(json => {
         const selectedIngredients = App.selectedIngredients
-        const matches = new Set()
+        const matches = []
 
         json.ingredients.forEach(ingredient => {
           selectedIngredients.forEach(partial => {
             if (ingredient.toLowerCase().indexOf(partial.toLowerCase()) >= 0) {
-              matches.add(ingredient)
+              matches.push(ingredient)
             }
           })
         })
 
-
         this.setState({
           matches,
-          product: json
+          product: json,
+          foodStatus: matches.length > 0 ? FoodStatuses.CannotEat : FoodStatuses.CanEat
         })
 
         this.props.navigation.setParams({
           title: json.desc
         })
       })
+      .catch(err => {
+        alert("Unable to resolve backend")
+      })
+  }
+
+  dismissFlash = () => {
+    this.setState({
+      flashDismissed: true
+    })
+  }
+
+  renderFlash = () => {
+    const { foodStatus } = this.state
+
+    let msg = (
+      <Text>
+        Loading
+        <Text></Text>
+      </Text>
+    )
+
+    if (foodStatus === FoodStatuses.CanEat) {
+      msg = (
+        <View>
+          <Icon
+            name='check-circle'
+            type='font-awesome'
+            color='green'
+            size={80}
+          />
+          <Text>No allergens found.</Text>
+        </View>
+      )
+    } else if (foodStatus === FoodStatuses.CannotEat) {
+      msg = (
+        <View>
+          <Icon
+            name='exclamation-circle'
+            type='font-awesome'
+            color='#c60017'
+            size={80}
+          />
+          <Text>Allergens found!</Text>
+        </View>
+      )
+    } else if (foodStatus === FoodStatuses.Unknown) {
+      msg = (
+        <View>
+          <Icon
+            name='question-circle'
+            type='font-awesome'
+            color='#517fa4'
+            size={80}
+          />
+          <Text>Unable to find that item...</Text>
+        </View>
+      )
+    }
+
+
+    if (this.state.flashDismissed)
+      return null
+
+    return (
+      <TouchableHighlight onPress={this.dismissFlash} style={styles.flash}>
+        {msg}
+      </TouchableHighlight>
+    )
   }
 
   renderIngredients = () => {
-    return Array.from(this.state.matches).map(item => {
+    return Array.from(new Set(this.state.matches)).map(item => {
+      return (
+        <ListItem
+          titleStyle={styles.badIngred}
+          key={item}
+          title={item}/>
+      )
+    })
+  }
+
+  renderAllIngredients = () => {
+    return this.state.product.ingredients.map(item => {
       return (
         <ListItem
           key={item}
@@ -86,7 +173,6 @@ export default class Home extends Component<{}> {
     return (
       <View style={styles.wrapper}>
         <ScrollView style={styles.container}>
-
           <Card
             style={styles.card}
             image={{uri: image}}>
@@ -95,7 +181,15 @@ export default class Home extends Component<{}> {
           <List style={styles.list}>
             {this.renderIngredients()}
           </List>
+          <View style={styles.allIngredTitle}>
+            <Text>All Ingredients</Text>
+            <List style={styles.list}>
+              {this.renderAllIngredients()}
+            </List>
+          </View>
         </ScrollView>
+
+        {!this.flashDismissed && this.renderFlash()}
       </View>
     );
   }
@@ -104,6 +198,9 @@ export default class Home extends Component<{}> {
 const styles = StyleSheet.create({
   wrapper: {
     height: "100%"
+  },
+  badIngred: {
+    color: 'red'
   },
   flash: {
     position: 'absolute',
@@ -121,6 +218,9 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: 'bold'
+  },
+  allIngredTitle: {
+    marginTop: 20
   },
   list: {
     padding: 0
